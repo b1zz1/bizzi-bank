5ec2c1a9dd35c3636129dbe7863ee0a652a40fd9
Eduardo Martins <eduardobizzi2005@gmail.com>
2024-03-01 10:52:43 -0300

    Project start
    Project has been structured and has initiated development.

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layouts/Container'
+import Navbar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Company" element={<Company />}  />
+          <Route exact path="/Contact" element={<Contact />} />
+          <Route exact path="/NewProject" element={<NewProject />} />
+        </Routes>
+      </Container>
+        <Footer />
+    </Router>
+  );
+}
+
+export default App;
